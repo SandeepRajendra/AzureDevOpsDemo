e638a5f5ac5307dc781041401b8a4abf17215867
sandeep rajendra <sandeep7979@outlook.com>
2021-05-15 21:28:49 +0530

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,57 @@
+﻿using AzureDevOpsDemo.Models;
+using AzureDevOpsDemo.Repository;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AzureDevOpsDemo.Controllers
+{
+	public class HomeController : Controller
+	{
+		//private readonly ILogger<HomeController> _logger;
+		IPostRepository postRepository;
+		//public HomeController(ILogger<HomeController> logger)
+		//{
+		//	_logger = logger;
+		//}
+		public HomeController(IPostRepository _postRepository)
+		{
+			postRepository = _postRepository;
+		}
+
+		public IActionResult Index()
+		{
+			var model = postRepository.GetPosts();
+			return View(model);
+		}
+		public IActionResult About()
+		{
+			ViewData["Message"] = "Your application description page.";
+			return View();
+		}
+		public IActionResult Contact()
+		{
+			ViewData["Message"] = "Your contact page.";
+			return View();
+		}
+		public IActionResult Privacy()
+		{
+			//var model = postRepository.GetPosts();
+			//return View(model);
+			return View();
+		}
+		
+
+		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+		public IActionResult Error()
+		{
+			return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+		}
+		
+		
+	}
+}
